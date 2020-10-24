e9d3b0c4a988a05e9bbb781ff4f26aa1268a78ff
Hnin Yee Nwe <hninyeenwe2@gmail.com>
2020-10-24 23:26:46 +0630

    first commit

---
+++ mingalarBarPyay.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">	
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<title>HSP Digital Marketing Agency</title>
+	<link rel="stylesheet" href="library/bootstrap/bootstrap.min.css">
+	<link rel="stylesheet" href="library/fontawesome/fontawesome-all.min.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="shortcut icon" href="images/16x16 px - png-logo.png">
+</head>
+<body>
+	<!-- navbar section  start-->
+	<nav class="navbar sticky-top navbar-expand-lg navbar-light navbar-fixed bg-light" id="navbar">
+		<a class="navbar-brand ml-5" href="index.php"><img src="images/87x87 - png - logo.png"></a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation">
+		    <span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse ml-5" id="navigation">
+			<ul class="navbar-nav nav-pills">
+				<li class="nav-item pr-4 mb-2">
+					<a href="index.php" class="nav-link active"><i class="fas fa-home mr-2"></i>ပင်မစာမျက်နှာ</a>
+				</li>
+		      <li class="nav-item pr-4 mb-2">
+		        <a class="nav-link text-white" href="#client"><i class="fas fa-user-friends mr-2"></i>သုံးစွဲသူများ  </a>
+		      </li>
+		      <li class="nav-item pr-4 mb-2">
+		        <a class="nav-link text-white" href="#aboutus"><i class="fas fa-user mr-2"></i>ကျွန်ုပ်တို့အကြောင်း</a>
+		      </li>
+		      <li class="nav-item pr-4 mb-2">
+		        <a class="nav-link text-white" href="#service"><i class="fas fa-cog mr-2"></i>ဝန်ဆောင်မှုများ</a>
+		      </li>
+		      <li class="nav-item pr-4 mb-2">
+		        <a class="nav-link text-white" href="#contact"><i class="fas fa-phone mr-2"></i>ဆက်သွယ်ရန်</a>
+		      </li>
+		      <li class="nav-item pr-4 mb-2">
+		        <a class="nav-link text-white" href="#blogvd"><i class="fas fa-th mr-2"></i>ဘလော့ခ်</a>
+		      </li>
+		    </ul>
+		</div>
+	</nav>
+	<!-- navbar section end -->
+
+	<section id="mingalarbar">
+		<div class="container">
+			<div class="row my-5">
+				<div class="col-md-9">
+					<div class="container mt-5 shadow-lg p-3 rounded" id="mingalar">
+			        	<h3 class="text-center">Mingalarbar Pyay</h3>
+					</div>
+				</div>
+				<div class="col-md-3">
+					<img src="images/Take a look at my Canva design! (1).png" class="img-fluid" alt="">
+				</div>
+			</div>
+			<div class="row my-5">
+				<div class="col-lg-3 col-md-3">
+					<img src="images/mingalarbar bed.jpg" class="img-fluid" alt="">
+				</div>
+				<div class="col-lg-3 col-md-3">
+					<img src="images/Mingalarbar Pyay-1.png" class="img-fluid" alt="">
+				</div>
+				<div class="col-lg-3 col-md-3">
+					<img src="images/Mingalarbar Pyay-2.png" class="img-fluid" alt="">
+				</div>
+				<div class="col-lg-3 col-md-3">
+					<img src="images/jolly.jpg" class="img-fluid" alt="">
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+	<!-- footer section start-->
+	<div id="contact">
+		<div class="container-fluid mb-2">
+			<div class="row">
+				<div class="col-lg-4">
+					<h4 class="text-center mt-4">တည်နေရာ:</h4><br>
+					<iframe  class="ml-5" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d807.6758215261694!2d95.21554196784714!3d18.819093632987403!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30c7835073d5366d%3A0x85271bdcd32f269a!2sBogyoke%20Road%2C%20Pye!5e1!3m2!1sen!2smm!4v1603184661535!5m2!1sen!2smm" width="300" height="250" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+				</div>
+				<div class="col-lg-4 mt-3 my-2">
+					<h4 class="text-center mt-3">ဆက်သွယ်ရန်အချက်အလက်:</h4>
+					<p class="font-rb font-size-16 mt-4 ml-4"><i class="fas fa-phone-alt mr-2"></i>09 893 199994</p>
+
+					<p class="font-rb font-size-16 ml-4"><a href="hsp.marketingagency@gmail.com" class="text-dark"><i class="far fa-envelope mr-2 "></i>hsp.marketingagency@gmail.com</a></p>
+
+					<p class="font-rb font-size-16 ml-4"><a href="https://web.facebook.com/hspdigitalmarketingagency" class="text-dark"><i class="fab fa-facebook-square mr-2"></i>HSP Digital Marketing Agency</a></p>
+
+					<p class="font-rb font-size-16 ml-4"><i class="fas fa-home mr-2"></i>No. 157,Bogyoke Road,Ywar Bae` East Ward,Near CB Bank (2),<br>Pyay,Bago Region,Myanmar,18156</p>
+				</div>
+				<div class="col-lg-4 mt-3 my-2">
+					<h4 class="text-center mt-3">ဆက်သွယ်ရန်ပုံစံ:</h4>
+					<form class="mt-4">
+						<input type="text" class="form-control " placeholder="အမည်">
+						<input type="email" class="form-control mt-3" placeholder="အီးမေးလ်">
+						<textarea cols="30" rows="3" class="form-control my-3" placeholder="မက်ဆေ့ချ်"></textarea>
+						<button class="btn btn-danger btn-block" type="submit">ဆက်သွယ်ပါ</button>
+					</form>
+				</div>
+			</div>
+		</div>
+		<footer class="text-muted bg-dark text-center">
+				copyright &copy;2020. All right reserved.		
+		</footer>
+	</div>
+	<!-- footer section end -->
