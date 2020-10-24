64188ec2bcb1e9c30c8c8aee6989747896a8b205
Hnin Yee Nwe <hninyeenwe2@gmail.com>
2020-10-24 12:22:25 +0630

    first commit

---
+++ index.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">	
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<title>HSP Digital Marketing Agency</title>
+	<link rel="stylesheet" href="library/bootstrap/bootstrap.min.css">
+	<link rel="stylesheet" href="library/fontawesome/fontawesome-all.min.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="shortcut icon" href="images/16x16 px - png-logo.png">
+</head>
+<body>
+	<!-- navbar section  start-->
+	<nav class="navbar sticky-top navbar-expand-lg navbar-light navbar-fixed bg-light" id="navbar">
+		<a class="navbar-brand ml-5" href="index.php"><img src="images/87x87 - png - logo.png"></a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation">
+		    <span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse ml-5" id="navigation">
+			<ul class="navbar-nav nav-pills">
+				<li class="nav-item pr-4 mb-2">
+					<a href="index.php" class="nav-link active"><i class="fas fa-home mr-2"></i>မူလစာမျက်နှာ</a>
+				</li>
+		      <li class="nav-item pr-4 mb-2">
+		        <a class="nav-link text-white" href="client.php"><i class="fas fa-user-friends mr-2"></i>သုံးစွဲသူများ</a>
+		      </li>
+		      <li class="nav-item pr-4 mb-2">
+		        <a class="nav-link text-white" href="aboutus.php"><i class="fas fa-user mr-2"></i>ကျွန်ုပ်တို့အကြောင်း</a>
+		      </li>
+		      <li class="nav-item pr-4 mb-2">
+		        <a class="nav-link text-white" href="service.php"><i class="fas fa-cog mr-2"></i>ဝန်ဆောင်မှုများ</a>
+		      </li>
+		      <li class="nav-item pr-4 mb-2">
+		        <a class="nav-link text-white" href="#contact"><i class="fas fa-phone mr-2"></i>ဆက်သွယ်ရန်</a>
+		      </li>
+		      <li class="nav-item pr-4 mb-2">
+		        <a class="nav-link text-white" href="blog.php"><i class="fas fa-th mr-2"></i>ဘလော့ခ်</a>
+		      </li>
+		    </ul>
+		</div>
+	</nav>
+	<!-- navbar section end -->
+
+	<!-- banner section start-->
+	<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+		<ol class="carousel-indicators">
+		    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+		    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+		    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+		</ol>
+		<div class="carousel-inner">
+		    <div class="carousel-item active">
+		      <img src="images/486x60-Full Banner.jpg" class="d-block w-100 img-fluid" alt="...">
+		    </div>
+		    <div class="carousel-item">
+		      <img src="images/486x60-Full Banner-1.jpg" class="d-block w-100 img-fluid" alt="...">
+		    </div>
+		    <div class="carousel-item">
+		      <img src="images/486x60-Full Banner-Confirm.jpg" class="d-block w-100 img-fluid" alt="...">
+		    </div>
+		</div>
+		<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+		    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+		    <span class="sr-only">Previous</span>
+		</a>
+		<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+		    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+		    <span class="sr-only">Next</span>
+		</a>
+	</div>
+	<!-- banner section end -->
+
+	<!-- client section start -->
+	<section id="client" class="mt-5">
+		<div class="container py-3 shadow-lg p-3 mb-5 rounded">
+			<h4 class="text-center my-3">သုံးစွဲသူများ</h4>
+			<div class="row">
+				<div class="col-md-3 mt-3">
+					<div class="client-top">
+							
+						<img src="images/Post-1-International Coffee Day.jpg" class="img-fluid" alt="">
+							
+					</div>
+					<div class="client-bottom mt-3 text-center">
+						<h3 class="mt-4 font-size-16 font-rb">Coffee kh</h3>
+					</div>
+				</div>
+				<div class="col-md-3 mt-3">
+					<div class="client-top">
+							
+						<img src="images/HP (2).jpg" class="img-fluid" alt="">
+							
+					</div>
+					<div class="client-bottom mt-3 text-center">
+						<h3 class="mt-4 font-size-16 font-rb">Herbal Paradise</h3>
+					</div>
+				</div>
+				<div class="col-md-3 mt-3">
+					<div class="client-top">
+							
+						<img src="images/cover.png" class="img-fluid" alt="">
+							
+					</div>
+					<div class="client-bottom mt-3 text-center">
+						<h3 class="mt-4 font-size-16 font-rb">Mingalarbar Pyay</h3>
+					</div>
+				</div>
+				<div class="col-md-3 mt-3">
+					<div class="client-top">
+						<img src="images/Post 1 - Welcome Post.jpg" class="img-fluid" alt="">
+					</div>
+					<div class="client-bottom mt-3 text-center">
+						<h3 class="mt-4 font-size-16 font-rb">Power Mobile</h3>
+					</div>
+				</div>
+				<div class="pagination">
+					<a href="client.php" class="next round">&#8250;</a>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- client section end -->
+
+	<!-- about section start -->
+	<div class="about">
+		<div class="container py-5">
+        	<h4 class="text-center">ကျွန်ုပ်တို့အကြောင်း</h4>
+			<div class="row mt-3 ">
+				<div class="col-lg-8">
+					<h4 class="py-3 font-rb">Facebook Marketing</h4>
+					<p class="font-rb">ကျွန်မတို့၏ ဆန်းသစ်သော Ideaများ၊ Digital Marketing နှင့် ပတ်သက်၍ ရရှိထားသော အတွေ့အကြုံများဖြင့် လူကြီးမင်းတို့၏ Product တွေ၊ Brand တွေကို Customerများ ဝယ်ယူအသုံး ပြုလာရန် ဖန်တီးပေးပြီး ပုံမှန် Facebook Follower များမှတစ်ဆင့် လူကြီးမင်းတို့၏ Product များကို အမှန်တစ်ကယ်သုံးစွဲသော Customer များဖြစ်လာစေရန် ကူညီဆောင်ရွက်ပေးနေပါသည်။..<a href="aboutus.php">See more</a></p>
+				</div>
+				<div class="col-lg-4">
+					<img src="images/Post-49.jpg" class="img-fluid" alt="">
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- about section end -->
+
+	<!-- our service section start-->
+	<section id="service" class="px-3 py-3">
+		<div class="container">
+			<h4 class="text-red text-center mt-3">ဝန်ဆောင်မှုများ</h4>
+			<div class="row mt-3">
+				<div class="col-md-6 col-lg-4">
+					<img src="images/services.jpg" alt="" class="img-fluid mt-3 ml-1">
+				</div>
+				<div class="col-md-6 col-lg-8 mt-5">
+					<ul>
+						<p class="font-rb font-size-16"><i class="fas fa-cog size-1x" style="color: #12abeb;"></i>
+						လူကြီးမင်းတို့၏ကုန်ပစ္စည်းများခင်းကျင်းပြသပေးခြင်း</p>
+						<p class="font-rb font-size-16"><i class="fas fa-cog" style="color: #12abeb;"></i>
+						သုံးရလွယ်ကူစေရန်ကုန်ပစ္စည်းများကိုဘယ်/ညာ‌ရွှေ့၍ရွေးချယ်နိုင်ခြင်း</p>
+						<p class="font-rb font-size-16"><i class="fas fa-cog" style="color: #12abeb;"></i>
+						လူကြီးမင်းတို့၏Customerများ သိလိုသမျှကို ၂၄နာရီလုံးလုံး Auto Reply Message စနစ်ဖြင့်ထားရှိပေးခြင်း</p>
+						<p class="font-rb font-size-16"><i class="fas fa-cog" style="color: #12abeb;"></i>
+						Content Calender များ ရေးဆွဲခြင်း</p>
+						<p class="font-rb font-size-16"><i class="fas fa-cog" style="color: #12abeb;"></i>
+						Creative Designs များ ရေးဆွဲခြင်း</p>
+						<p class="font-rb font-size-16"><i class="fas fa-cog" style="color: #12abeb;"></i>
+						Facebook Ads များ ‌ရွေးချယ်ခြင်း</p>
+						<a href="service.php" class="mt-3 ml-5 btn btn-danger">ပိုမိုသိရှိရန်</a>
+					</ul>
+				</div>
+			</div>
+		</div>	
+	</section>
+	<!-- our service section end -->
+
+	<!-- blog section start -->
+	<div id="blogvd">
+		<div class="container-fluid py-5" style="background-color: #88d4f5;">
+			<div class="row">
+				<div class="col-md-6">
+					<h5 class="my-5 ml-4">သင့်လုပ်ငန်းတွေအောင်မြင်ဖို့ HSP နဲ့ချိတ်ဆက်စို့...</h5>
+				</div>	
+				<div class="col-md-6">
+					<video width="350" height="200" class="ml-4" controls>
+			  			<source src="images/COVER-HSP.mp4" type="video/mp4">
+					</video>	
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- blog section end -->
+
+	<!-- footer section start-->
+	<div id="contact">
+		<div class="container-fluid mb-2">
+			<div class="row">
+				<div class="col-lg-4">
+					<h4 class="text-center mt-4">တည်နေရာ:</h4><br>
+					<iframe  class="ml-5" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d807.6758215261694!2d95.21554196784714!3d18.819093632987403!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30c7835073d5366d%3A0x85271bdcd32f269a!2sBogyoke%20Road%2C%20Pye!5e1!3m2!1sen!2smm!4v1603184661535!5m2!1sen!2smm" width="300" height="250" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+				</div>
+				<div class="col-lg-4 mt-3 my-2">
+					<h4 class="text-center mt-3">ဆက်သွယ်ရန်အချက်အလက်:</h4>
+					<p class="font-rb font-size-16 mt-4 ml-4"><i class="fas fa-phone-alt mr-2"></i>09 893 199994</p>
+
+					<p class="font-rb font-size-16 ml-4"><a href="hsp.marketingagency@gmail.com" class="text-dark"><i class="far fa-envelope mr-2 "></i>hsp.marketingagency@gmail.com</a></p>
+
+					<p class="font-rb font-size-16 ml-4"><a href="https://web.facebook.com/hspdigitalmarketingagency" class="text-dark"><i class="fab fa-facebook-square mr-2"></i>HSP Digital Marketing Agency</a></p>
+
+					<p class="font-rb font-size-16 ml-4"><i class="fas fa-home mr-2"></i>No. 157,Bogyoke Road,Ywar Bae` East Ward,Near CB Bank (2),<br>Pyay,Bago Region,Myanmar,18156</p>
+				</div>
+				<div class="col-lg-4 mt-3 my-2">
+					<h4 class="text-center mt-3">ဆက်သွယ်ရန်ပုံစံ:</h4>
+					<form class="mt-4">
+						<input type="text" class="form-control " placeholder="အမည်">
+						<input type="email" class="form-control mt-3" placeholder="အီးမေးလ်">
+						<textarea cols="30" rows="3" class="form-control my-3" placeholder="မက်ဆေ့ချ်"></textarea>
+						<button class="btn btn-danger btn-block" type="submit">မက်ဆေ့ချ်ပို့ပါ</button>
+					</form>
+				</div>
+			</div>
+		</div>
+		<footer class="text-muted bg-dark text-center">
+				copyright &copy;2020. All right reserved.		
+		</footer>
+	</div>
+	<!-- footer section end -->
+
+	<script src="library/bootstrap/jquery-3.4.1.slim.min.js"></script>
+	<script src="library/bootstrap/popper.min.js"></script>
+	<script src="library/bootstrap/bootstrap.min.js"></script>
+	<script src="library/typed-js/typed.min.js"></script>
+	<script src="js/script.js"></script>
+</body>
+</html>
